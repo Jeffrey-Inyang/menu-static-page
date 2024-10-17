9baa11457fa553ad99fbd6790db6ed2a379a8f2f
samuel-eke <eke.eke.eth@gmail.com>
2024-10-17 22:20:01 +0100

    1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import AboutState from "./learning/AboutState";
-import styles from "../src/styles/secondstyle.module.css";
 import Menu from "./components/Menu";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Pizza({ pizzaObj }) {
   console.log(pizzaObj);
 
